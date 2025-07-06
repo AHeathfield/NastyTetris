d8fec7d65cdf89c195d9abd2b04e4c69d6928ce5
Aidan <aidan.heathfield@gmail.com>
2025-07-06 17:05:04 -0400

    Moved LTexture class def to another file

--- game.cpp
+++ game.cpp
@@ -11,6 +11,8 @@
 #include <string>
 #include <sstream>
 
+#include "src/Classes/LTexture.h"
+
 /* Constants */
 //Screen dimension constants
 constexpr int kScreenWidth{ 640 };
@@ -184,54 +186,7 @@ class LButton
 
 
 /* Class Prototypes */
-class LTexture
-{
-public:
-    // Symbolic constant
-    static constexpr float kOriginalSize = -1.f;
-    
-    //Initializes texture variables
-    LTexture();
 
-    //Cleans up texture variables
-    ~LTexture();
-
-    //Loads texture from disk
-    bool loadFromFile( std::string path );
-
-    #if defined(SDL_TTF_MAJOR_VERSION)
-    //Creates texture from text
-    bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
-    #endif
-
-    //Cleans up texture
-    void destroy();
-
-     //Sets color modulation
-    void setColor( Uint8 r, Uint8 g, Uint8 b);
-
-    //Sets opacity
-    void setAlpha( Uint8 alpha );
-
-    //Sets blend mode
-    void setBlending( SDL_BlendMode blendMode );
-
-    //Draws texture
-    void render( float x, float y, SDL_FRect* clip = nullptr, float width = kOriginalSize, float height = kOriginalSize, double degrees = 0.0, SDL_FPoint* center = nullptr, SDL_FlipMode flipMode = SDL_FLIP_NONE );
-
-    //Gets texture attributes
-    int getWidth();
-    int getHeight();
-    bool isLoaded();
-
-private:
-    //Contains texture data
-    SDL_Texture* mTexture;
-
-    //Texture dimensions
-    int mWidth;
-    int mHeight;
-};
 
 
 
@@ -399,7 +354,7 @@ void Dot::move()
 void Dot::render( SDL_FRect camera )
 {
     //Show the dot
-    gDotTexture.render( static_cast<float>( mPosX ) - camera.x, static_cast<float>( mPosY ) - camera.y );
+    gDotTexture.render(gRenderer, static_cast<float>( mPosX ) - camera.x, static_cast<float>( mPosY ) - camera.y );
 }
 
 int Dot::getPosX()
@@ -606,126 +561,7 @@ void LButton::render()
 
 
 //LTexture Implementation
-LTexture::LTexture():
-    //Initialize texture variables
-    mTexture{ nullptr },
-    mWidth{ 0 },
-    mHeight{ 0 }
-{
 
-}
-
-LTexture::~LTexture()
-{
-    //Clean up texture
-    destroy();
-}
-
-bool LTexture::loadFromFile( std::string path )
-{
-    //Clean up texture if it already exists
-    destroy();
-
-    //Load surface
-    if( SDL_Surface* loadedSurface = IMG_Load( path.c_str() ); loadedSurface == nullptr )
-    {
-        SDL_Log( "Unable to load image %s! SDL_image error: %s\n", path.c_str(), SDL_GetError() );
-    }
-    else
-    {
-        //Color key image
-        if( SDL_SetSurfaceColorKey( loadedSurface, true, SDL_MapSurfaceRGB( loadedSurface, 0x00, 0xFF, 0xFF ) ) == false )
-        {
-            SDL_Log( "Unable to color key! SDL error: %s", SDL_GetError() );
-        }
-        else
-        {
-            //Create texture from surface
-            if( mTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface ); mTexture == nullptr )
-            {
-                SDL_Log( "Unable to create texture from loaded pixels! SDL error: %s\n", SDL_GetError() );
-            }
-            else
-            {
-                //Get image dimensions
-                mWidth = loadedSurface->w;
-                mHeight = loadedSurface->h;
-            }
-        }
-        
-        //Clean up loaded surface
-        SDL_DestroySurface( loadedSurface );
-    }
-
-    //Return success if texture loaded
-    return mTexture != nullptr;
-}
-
-
-int LTexture::getWidth()
-{
-    return mWidth;
-}
-
-int LTexture::getHeight()
-{
-    return mHeight;
-}
-
-bool LTexture::isLoaded()
-{
-    return mTexture != nullptr;
-}
-
-void LTexture::destroy()
-{
-    //Clean up texture
-    SDL_DestroyTexture( mTexture );
-    mTexture = nullptr;
-    mWidth = 0;
-    mHeight = 0;
-}
-
-void LTexture::render( float x, float y, SDL_FRect* clip, float width, float height, double degrees, SDL_FPoint* center, SDL_FlipMode flipMode )
-{
-    //Set texture position
-    SDL_FRect dstRect{ x, y, static_cast<float>( mWidth ), static_cast<float>( mHeight ) };
-
-    //Default to clip dimensions if clip is given
-    if( clip != nullptr )
-    {
-        dstRect.w = clip->w;
-        dstRect.h = clip->h;
-    }
-
-    //Resize if new dimensions are given
-    if( width > 0 )
-    {
-        dstRect.w = width;
-    }
-    if( height > 0 )
-    {
-        dstRect.h = height;
-    }
-
-    //Render texture
-    SDL_RenderTextureRotated( gRenderer, mTexture, clip, &dstRect, degrees, center, flipMode );
-}
-
-void LTexture::setColor( Uint8 r, Uint8 g, Uint8 b )
-{
-    SDL_SetTextureColorMod( mTexture, r, g, b );
-}
-
-void LTexture::setAlpha( Uint8 alpha )
-{
-    SDL_SetTextureAlphaMod( mTexture, alpha );
-}
-
-void LTexture::setBlending( SDL_BlendMode blendMode )
-{
-    SDL_SetTextureBlendMode( mTexture, blendMode );
-}
 
 #if defined(SDL_TTF_MAJOR_VERSION)
 bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
@@ -859,12 +695,12 @@ bool loadMedia()
 
 
     //Load scene images
-    if( gDotTexture.loadFromFile( "src/Assets/dot.png" ) == false )
+    if( gDotTexture.loadFromFile(gRenderer, "src/Assets/dot.png" ) == false )
     {
         SDL_Log( "Unable to load foo image!\n");
         success = false;
     }
-    if( gBgTexture.loadFromFile( "src/Assets/testBG.png" ) == false )
+    if( gBgTexture.loadFromFile(gRenderer, "src/Assets/testBG.png" ) == false )
     {
         SDL_Log( "Unable to load foo image!\n");
         success = false;
@@ -1305,7 +1141,7 @@ int main( int argc, char* args[] )
                 SDL_RenderClear( gRenderer );
 
                 // Render Texture
-                gBgTexture.render(0.f, 0.f, &camera);
+                gBgTexture.render(gRenderer, 0.f, 0.f, &camera);
 
                 //Render dot
                 dot.render( camera );
---
+++ src/Classes/LTexture.cpp
@@ -0,0 +1,122 @@
+#include "LTexture.h"
+
+LTexture::LTexture():
+    //Initialize texture variables
+    mTexture{ nullptr },
+    mWidth{ 0 },
+    mHeight{ 0 }
+{
+
+}
+
+LTexture::~LTexture()
+{
+    //Clean up texture
+    destroy();
+}
+
+bool LTexture::loadFromFile( SDL_Renderer* renderer, std::string path )
+{
+    //Clean up texture if it already exists
+    destroy();
+
+    //Load surface
+    if( SDL_Surface* loadedSurface = IMG_Load( path.c_str() ); loadedSurface == nullptr )
+    {
+        SDL_Log( "Unable to load image %s! SDL_image error: %s\n", path.c_str(), SDL_GetError() );
+    }
+    else
+    {
+        //Color key image
+        if( SDL_SetSurfaceColorKey( loadedSurface, true, SDL_MapSurfaceRGB( loadedSurface, 0x00, 0xFF, 0xFF ) ) == false )
+        {
+            SDL_Log( "Unable to color key! SDL error: %s", SDL_GetError() );
+        }
+        else
+        {
+            //Create texture from surface
+            if( mTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface ); mTexture == nullptr )
+            {
+                SDL_Log( "Unable to create texture from loaded pixels! SDL error: %s\n", SDL_GetError() );
+            }
+            else
+            {
+                //Get image dimensions
+                mWidth = loadedSurface->w;
+                mHeight = loadedSurface->h;
+            }
+        }
+        
+        //Clean up loaded surface
+        SDL_DestroySurface( loadedSurface );
+    }
+
+    //Return success if texture loaded
+    return mTexture != nullptr;
+}
+
+
+int LTexture::getWidth()
+{
+    return mWidth;
+}
+
+int LTexture::getHeight()
+{
+    return mHeight;
+}
+
+bool LTexture::isLoaded()
+{
+    return mTexture != nullptr;
+}
+
+void LTexture::destroy()
+{
+    //Clean up texture
+    SDL_DestroyTexture( mTexture );
+    mTexture = nullptr;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+void LTexture::render(SDL_Renderer* renderer, float x, float y, SDL_FRect* clip, float width, float height, double degrees, SDL_FPoint* center, SDL_FlipMode flipMode )
+{
+    //Set texture position
+    SDL_FRect dstRect{ x, y, static_cast<float>( mWidth ), static_cast<float>( mHeight ) };
+
+    //Default to clip dimensions if clip is given
+    if( clip != nullptr )
+    {
+        dstRect.w = clip->w;
+        dstRect.h = clip->h;
+    }
+
+    //Resize if new dimensions are given
+    if( width > 0 )
+    {
+        dstRect.w = width;
+    }
+    if( height > 0 )
+    {
+        dstRect.h = height;
+    }
+
+    //Render texture
+    SDL_RenderTextureRotated( renderer, mTexture, clip, &dstRect, degrees, center, flipMode );
+}
+
+void LTexture::setColor( Uint8 r, Uint8 g, Uint8 b )
+{
+    SDL_SetTextureColorMod( mTexture, r, g, b );
+}
+
+void LTexture::setAlpha( Uint8 alpha )
+{
+    SDL_SetTextureAlphaMod( mTexture, alpha );
+}
+
+void LTexture::setBlending( SDL_BlendMode blendMode )
+{
+    SDL_SetTextureBlendMode( mTexture, blendMode );
+}
