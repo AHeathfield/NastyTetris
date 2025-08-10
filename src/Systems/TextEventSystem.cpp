3009c4aa56aebe838c73d7c3ea88621aa7c122d4
Aidan <aidan.heathfield@gmail.com>
2025-08-10 19:28:47 -0400

    Can now save highscores

--- src/Systems/TextEventSystem.cpp
+++ src/Systems/TextEventSystem.cpp
@@ -2,45 +2,68 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_keycode.h>
 #include <cstddef>
-// #include <iostream>
 #include <string>
+// #include <iostream>
+// #include <string>
 
 extern Coordinator gCoordinator;
 
 // Helper method
-// int convertScoreSTRtoINT(std::string scoreSTR)
-// {
-//     std::string temp;
-//     for (int i = 0; i < scoreSTR.length(); i++)
-//     {
-//         if (!scoreSTR[i] == '0')
-//         {
-//             temp = scoreSTR[i];
-//         }
-//     }
-//
-//     return std::stoi(temp);
-// }
+int convertScoreSTRtoINT(std::string scoreSTR)
+{
+    std::string temp;
+    for (int i = 0; i < scoreSTR.length(); i++)
+    {
+        // SDL_Log(&scoreSTR[i]);
+        if (scoreSTR[i] != '0')
+        {
+            temp += scoreSTR[i];
+        }
+    }
+
+    if (temp.length() == 0)
+    {
+        // SDL_Log("LENGTH 0");
+        temp = "0";
+    }
+
+    int score = std::stoi(temp);
+    return score;
+}
 
 void TextEventSystem::Init()
 {
     mName = "AAA";
     mIsFinalScoreHigh = false;
     auto scoreSystem = gCoordinator.GetSystem<ScoreSystem>();
+    // SDL_Log(scoreSystem->GetScoreString().c_str());
 
     // Opening score file in read mode
     std::ifstream file("src/Scores.txt");
-    std::string line;
     
+    if (!file.is_open())
+    {
+        SDL_Log("Unable to open file!");
+    }
+
+    std::string line;
     // Creates entities to hold the text to render
     int count = 1;
     while (getline(file, line) && count < 11)
     {
         // This places the final score in correct spot
-        std::string scoreStr = line.substr(4, 8);
+        std::string scoreStr;
+        if (scoreStr.substr(0, 2) == "10")
+        {
+            scoreStr = line.substr(8, 8);
+        }
+        else
+        {
+            scoreStr = line.substr(7, 8);
+        }
 
         // Place the mTextField here
-        if (scoreSystem->score >= std::stoi(scoreStr) && mIsFinalScoreHigh == false)
+        if (scoreSystem->score >= convertScoreSTRtoINT(scoreStr) && mIsFinalScoreHigh == false)
         {
             mIsFinalScoreHigh = true;
             // SDL_Log("IT WORKED");
@@ -53,50 +76,60 @@ void TextEventSystem::Init()
                         .path = "src/Assets/8bit16.ttf",
                         .isText = true,
                         .fontSize = 32,
-                        .text = std::to_string(count) + ". AAA " + scoreStr,
+                        .text = std::to_string(count) + ". AAA " + scoreSystem->GetScoreString(),
                     });
             gCoordinator.AddComponent(
                     mTextField, 
                     TransformComponent{
-                        // .position = Vector2(888.f, 260.f)
                         .position = Vector2(780.f, 260.f + (64 * count))
                     });
             mScores.push_back(mTextField);
+            count++;
+        }
+
+
+        std::string nameAndScore;
+        if (scoreStr.substr(0, 2) == "10")
+        {
+            SDL_Log("10");
+            nameAndScore = line.substr(4);
         }
         else
         {
-
-            // This creates the text entity for the score
-            Entity score = gCoordinator.CreateEntity();
-            score = gCoordinator.CreateEntity();
-            gCoordinator.AddComponent(
-                    score,
-                    TextureComponent{
-                        .texture = nullptr,
-                        .path = "src/Assets/8bit16.ttf",
-                        .isText = true,
-                        .fontSize = 32,
-                        .text = std::to_string(count) + ". " + line,
-                    });
-            gCoordinator.AddComponent(
-                    score, 
-                    TransformComponent{
-                        .position = Vector2(780.f, 260.f + (64 * count))
-                    });
-            mScores.push_back(score);
+            nameAndScore = line.substr(3);
         }
+
+        // This creates the text entity for the score
+        Entity score = gCoordinator.CreateEntity();
+        score = gCoordinator.CreateEntity();
+        gCoordinator.AddComponent(
+                score,
+                TextureComponent{
+                    .texture = nullptr,
+                    .path = "src/Assets/8bit16.ttf",
+                    .isText = true,
+                    .fontSize = 32,
+                    .text = std::to_string(count) + ". " + nameAndScore,
+                });
+        gCoordinator.AddComponent(
+                score, 
+                TransformComponent{
+                    .position = Vector2(780.f, 260.f + (64 * count))
+                });
+        mScores.push_back(score);
 
         count++;
     }
 
-    if (file.eof())
-    {
-        SDL_Log("REACHED END OF FILE");
-    }
-    else
-    {
-        SDL_Log("Error reading file");
-    }
+    // We don't always go to the end of file
+    // if (file.eof())
+    // {
+    //     SDL_Log("REACHED END OF FILE");
+    // }
+    // else
+    // {
+    //     SDL_Log("Error reading file");
+    // }
 
     file.close();
 
@@ -158,7 +191,15 @@ void TextEventSystem::HandleEvent(SDL_Event e)
         if (isNameChanged)
         {
             auto scoreSystem = gCoordinator.GetSystem<ScoreSystem>();
-            texture.text = texture.text.substr(0, 3) + mName + " " + scoreSystem->GetScoreString();
+
+            if (texture.text.substr(0, 2) == "10")
+            {
+                texture.text = texture.text.substr(0, 4) + mName + " " + scoreSystem->GetScoreString();
+            }
+            else
+            {
+                texture.text = texture.text.substr(0, 3) + mName + " " + scoreSystem->GetScoreString();
+            }
             auto renderSystem = gCoordinator.GetSystem<RenderSystem>();
             renderSystem->LoadMedia(&texture);
         }
@@ -167,6 +208,9 @@ void TextEventSystem::HandleEvent(SDL_Event e)
 
 void TextEventSystem::Close()
 {
+    // Saves the score to file
+    Save();
+
     gCoordinator.GetComponent<TextureComponent>(mFinalScore).destroy();
     gCoordinator.DestroyEntity(mFinalScore);
 
@@ -177,4 +221,20 @@ void TextEventSystem::Close()
     }
 
     mScores.clear();
+}
+
+
+// Private methods
+void TextEventSystem::Save()
+{
+    // Opening file
+    std::ofstream file("src/Scores.txt");
+
+    for (int i = 0; i < mScores.size(); i++)
+    {
+        const auto& texture = gCoordinator.GetComponent<TextureComponent>(mScores[i]);
+        file << texture.text << std::endl;
+    }
+
+    file.close();
 }
